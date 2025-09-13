5e2a2e50ff9944842bbd68c69612dea69278cce6
Artyom Vasilyev <hegelianer@gmail.com>
2025-09-13 13:51:38 +0700

    all migrations

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -4,7 +4,7 @@ INSERT INTO product (id, name, picture_url, price) VALUES
 (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 225),
 (4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315),
 (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 330),
-(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 189)
+(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 189);
 
 INSERT INTO orders (id, status, date_created) VALUES
 (1, 'cancelled', '2025-08-21'),
@@ -13,7 +13,7 @@ INSERT INTO orders (id, status, date_created) VALUES
 (4, 'shipped', '2025-08-24'),
 (5, 'shipped', '2025-08-25'),
 (6, 'shipped', '2025-08-26'),
-(7, 'shipped', '2025-08-27')
+(7, 'shipped', '2025-08-27');
 
 INSERT INTO order_product (order_id, product_id, quantity) VALUES
 (1, 4, 33),
@@ -23,4 +23,4 @@ INSERT INTO order_product (order_id, product_id, quantity) VALUES
 (4, 5, 15),
 (5, 6, 25),
 (6, 1, 12),
-(7, 4, 18)
+(7, 4, 18);
